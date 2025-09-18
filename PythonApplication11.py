b7e2fe56ab7645efb644c82e59d5c58c770371c3
anastasiaserbatuk87-beep <anastasiaserbatuk87@gmail.com>
2025-09-18 22:50:52 +0300

    Add files via upload

---
+++ PythonApplication11.py
@@ -0,0 +1,9 @@
+﻿import math
+x = int(input("Введіть двоцифрове число:"))
+a = x // 10
+b = x % 10
+d = [a, b]
+c = [i * 100 + j * 10 + k for i in d for j in d for k in d]
+print("Квадрати чисел: ")
+for n in c:
+    print("{0}^2 = {1}".format(n,pow(n, 2)))
